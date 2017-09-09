e97bb8f95a3932246491f934283c0feea82ec55b
CalebBlack <caleb@polished.tech>
2017-09-08 20:58:11 -0600

    Update app.js

--- packages/react-scripts/template/server/app.js
+++ packages/react-scripts/template/server/app.js
@@ -6,13 +6,15 @@ const PORT = process.env.PORT || 9000;
 //END OF CONSTS
 //MAIN PROGRAM
 const app = express();
+// Enable Helmet for more protection
+app.use(helmet());
 
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
+// Serve routes
+Router(app);
 
 // Serve webpage
 app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))});
-// Serve routes
-Router(app);
-app.disable('x-powered-by');
+// Start Server
 app.listen(PORT,()=>{console.log('Server running on port #'+PORT);});
