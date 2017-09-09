355c5c570f92bb0ad03732317803bab202f76c45
CalebBlack <caleb@polished.tech>
2017-09-09 15:40:17 -0600

    Update birthday.js

--- packages/react-scripts/template/server/routes/birthday.js
+++ packages/react-scripts/template/server/routes/birthday.js
@@ -1,5 +1,16 @@
 const validateAuthToken = require('../functions/validateauthtoken');
+const response = require('../functions/response');
+const find = require('../functions/findinmodel');
+
 function get(req,res,models){
-  res.send('wip');
+  validateAuthToken(req).then(token=>{
+    find(models.User,{username:token.owner}).then(user=>{
+      response.success(res,user.created);
+    }).catch(err=>{
+      response.internal(res);
+    });
+  }).catch(err=>{
+    response.error(res,'Invalid Auth');
+  });
 }
 module.exports = {get};
