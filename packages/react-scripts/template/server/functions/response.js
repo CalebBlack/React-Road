38550156d472ef6654b5026f769fa5d49812e3a1
CalebBlack <caleb@polished.tech>
2017-09-09 02:47:35 -0600

    Update response.js

--- packages/react-scripts/template/server/functions/response.js
+++ packages/react-scripts/template/server/functions/response.js
@@ -1,7 +1,9 @@
-function success(res,details) {
+function success(res,details,status=200) {
+  res.status(status);
   res.json({status:'success',details});
 }
-function error(res,details){
+function error(res,details,status=400){
+  res.status(status);
   res.json({status:'error',details});
 }
 module.exports = {success,error};
