afe7e606fec680eb9b2d3dfdc5109fb835f7f342
CalebBlack <caleb@polished.tech>
2017-09-09 04:19:00 -0600

    Update user.js

--- packages/react-scripts/template/server/schemas/user.js
+++ packages/react-scripts/template/server/schemas/user.js
@@ -3,7 +3,6 @@ module.exports = {
   displayname: {type:String,required:true},
   email: {type:String,required:true},
   admin: {type:Boolean,default:false,required:true},
-  salt: {type:String,required:true},
   hash: {type:String,required:false},
   created: { type: Date, default: Date.now }
 }
