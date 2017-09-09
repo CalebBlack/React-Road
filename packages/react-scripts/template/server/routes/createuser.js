bc59cf069f65ad6e1af4eb796fb36beb8585f09b
CalebBlack <caleb@polished.tech>
2017-09-09 14:54:17 -0600

    Update createuser.js

--- packages/react-scripts/template/server/routes/createuser.js
+++ packages/react-scripts/template/server/routes/createuser.js
@@ -6,7 +6,7 @@ function post(req,res,models){
     if (req.body.email && req.body.username && req.body.password ) {
       var userInput = sanitize(req.body,{email:'email',username:'username',password:'password'});
       if (userInput.email && userInput.username && userInput.password) {
-        userManager.create(models,userInput.username,userInput.password,userInput.email).then(user=>{
+        userManager.signup(models,userInput.username,userInput.password,userInput.email).then(user=>{
           response.success(res,'Account Created');
         }).catch(err=>{
           response.internal(res);
