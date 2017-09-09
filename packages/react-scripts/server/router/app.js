e3c08b1ed263c27921e85fafa87c9e2fadbf9ac0
CalebBlack <caleb@polished.tech>
2017-09-08 20:46:31 -0600

    Update app.js

--- packages/react-scripts/server/router/app.js
+++ packages/react-scripts/server/router/app.js
@@ -1,8 +1,25 @@
 const routemap = require('./routes/routemap.js');
-function setupRouter(app) {
-  for (var route in routemap) {
-    if (route[get]) {
-      
+function setupRouter(app,APIRoute="") {
+  if (typeof APIRoute !== "string"){
+    throw new Error("Invalid API Route, got "+APIRoute+", but a string is required.");
+  }
+  for (var routename in routemap) {
+    var route = routemap[routename];
+    var path = route.url || routename;
+    if (route.get) {
+      app.get(APIRoute+path,route.get);
+    }
+    if (route.post) {
+      app.post(APIRoute+path,route.create);
+    }
+    if (route.create) {
+      app.create(APIRoute+path,route.get);
+    }
+    if (route.delete) {
+      app.delete(APIRoute+path,route.delete
+    }
+    if (route.put) {
+      app.put(APIRoute+path,route.delete)
     }
   }
 }
