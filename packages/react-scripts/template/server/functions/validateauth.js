38db12131688f8b15cff963d3c166751b2301ea0
CalebBlack <caleb@polished.tech>
2017-09-09 04:38:50 -0600

    Create validateauth.js

---
+++ packages/react-scripts/template/server/functions/validateauth.js
@@ -0,0 +1,27 @@
+const decodeAuth = require('../functions/decodeauth');
+
+module.exports = function(req,models) {
+  return new Promise((resolve,reject)=>{
+    var auth = decodeAuth(req);
+    if (!auth) {
+      reject('Improperly Formatted Auth');
+    }
+    models.User.find({username:auth.username.toLowerCase()}).then(users=>{
+      if (users[0]){
+        var user = users[0];
+        console.log(auth);
+        bcrypt.compare(auth.password, user.hash, function(err, match) {
+          if (err || match !== true) {
+              reject(err);
+            } else {
+              resolve(user);
+            }
+        });
+      } else {
+        reject("User not found.");
+      }
+    }).catch(err=>{
+      reject(err);
+    });
+  });
+}
