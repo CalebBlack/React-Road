9c67d80d1050dc1edbab3387b263d2834b687260
CalebBlack <caleb@polished.tech>
2017-09-08 23:21:22 -0600

    Update sanitize.js

--- packages/react-scripts/template/server/functions/sanitize.js
+++ packages/react-scripts/template/server/functions/sanitize.js
@@ -1,7 +1,14 @@
 function sanitize(query,format){
   var output = null;
-  if (typeof query === format || (typeof query === 'object' && typeof format === 'object')) {
-    if (typeof query === 'object') {
+  if (typeof query === format || (typeof query === 'object' && (typeof format === 'object' || format === "array"))) {
+    if (Array.isArray(query) && format === 'array'){
+      output = [];
+      for (var i = 0; i < query.length; i++) {
+        if (typeof query[i] !== 'object' && typeof query[i] !== 'function') {
+          output[i] = query[i];
+        }
+      }
+    } else if (typeof query === 'object') {
       output = {};
       for (var queryProp in query) {
         if (format[queryProp]) {
@@ -17,4 +24,3 @@ function sanitize(query,format){
   }
   return output;
 }
-module.exports = sanitize;
