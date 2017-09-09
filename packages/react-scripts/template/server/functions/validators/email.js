859dbb73d6d15efc44b8c384af9493e9992277dc
CalebBlack <caleb@polished.tech>
2017-09-09 00:47:34 -0600

    Update email.js

--- packages/react-scripts/template/server/functions/validators/email.js
+++ packages/react-scripts/template/server/functions/validators/email.js
@@ -1,1 +1,4 @@
-
+function validateEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+module.exports = validateEmail;
