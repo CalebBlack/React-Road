0e96ab534fe00f6932a6c813754e457b4dfa4740
CalebBlack <caleb@polished.tech>
2017-09-09 14:20:03 -0600

    Update users.js

--- packages/react-scripts/template/server/managers/users.js
+++ packages/react-scripts/template/server/managers/users.js
@@ -1,5 +1,6 @@
+const bcrypt = require('bcrypt');
 function create(models,username,password,email){
-
+  var userData = {username:username.toLowerCase(),displayname:username,email};
 }
 function login(models,username,password){
 
