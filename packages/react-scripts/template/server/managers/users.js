528b1ea2f0ffd71802b319a10af30e766208567c
CalebBlack <caleb@polished.tech>
2017-09-09 14:36:46 -0600

    Update users.js

--- packages/react-scripts/template/server/managers/users.js
+++ packages/react-scripts/template/server/managers/users.js
@@ -46,6 +46,58 @@ function genToken(models,username){
     });
   });
 }
+function signup(models,username,email,password) {
+  return new Promise((resolve,reject)=>{
+    genHash(userInput.password).then(hash=>{
+      var user = new models.User({username:username.toLowerCase(),displayname:username,email,hash});
+      user.save().then(()=>{
+        resolve(user);
+      }).catch(err=>{
+        reject(err);
+      });
+    }).catch(err=>{
+      reject(err);
+    });
+  });
+}
+/*
 
+function post(req,res,models){
+  if (req.body) {
+    if (req.body.email && req.body.username && req.body.password ) {
+      var userInput = sanitize(req.body,{email:'email',username:'username',password:'password'});
+      if (userInput.email && userInput.username && userInput.password) {
+        userInput.displayname = userInput.username;
+        userInput.username = userInput.username.toLowerCase();
+        userInput.email = userInput.email.toLowerCase();
+        genHash(userInput.password).then(hash=>{
+          userInput.hash = hash;
+          var user = new models.User(userInput);
+          user.save(err=>{
+            if (err) {
+              console.log(err);
+              response.error(res,'Creation Error');
+            } else {
+              response.success(res);
+            }
+          });
+        }).catch((err)=>{
+          console.log(err);
+          response.error(res,'Creation Error');
+        });
+      } else {
+        response.error(res,'Invalid Parameters');
+      }
+    } else {
+      response.error(res,'Missing Parameters');
+    }
+  } else {
+    response.error(res,'Missing Body');
+  }
+}
+module.exports = {post}
+
+
+*/
 
 module.exports = {login,create};
