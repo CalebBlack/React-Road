1a73daf54d9acb7bf39e1fa9e3e4b53c8c2f2547
CalebBlack <caleb@polished.tech>
2017-09-09 02:28:52 -0600

    Create decodeauth.js

---
+++ packages/react-scripts/template/server/functions/decodeauth.js
@@ -0,0 +1,10 @@
+module.exports = function(req) {
+  if (!req || !req.headers || !req.headers.authorization || typeof req.headers.authorization !== "string") {
+    return null;
+  }
+  auth = req.headers.authorization;
+  if (!auth.startsWith('Basic ')) {
+    return null;
+  }
+  return Buffer.from(auth.substring(6), 'base64').toString('utf8'));
+}
