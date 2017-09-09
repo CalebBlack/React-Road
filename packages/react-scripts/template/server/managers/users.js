f69160be5b62c0b3feada92ec8202195d5384321
CalebBlack <caleb@polished.tech>
2017-09-09 14:37:21 -0600

    Update users.js

--- packages/react-scripts/template/server/managers/users.js
+++ packages/react-scripts/template/server/managers/users.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const genHash = require('../functions/genhash');
 function create(models,username,password,email){
   var userData = {username:username.toLowerCase(),displayname:username,email};
 }
@@ -60,44 +61,6 @@ function signup(models,username,email,password) {
     });
   });
 }
-/*
 
-function post(req,res,models){
-  if (req.body) {
-    if (req.body.email && req.body.username && req.body.password ) {
-      var userInput = sanitize(req.body,{email:'email',username:'username',password:'password'});
-      if (userInput.email && userInput.username && userInput.password) {
-        userInput.displayname = userInput.username;
-        userInput.username = userInput.username.toLowerCase();
-        userInput.email = userInput.email.toLowerCase();
-        genHash(userInput.password).then(hash=>{
-          userInput.hash = hash;
-          var user = new models.User(userInput);
-          user.save(err=>{
-            if (err) {
-              console.log(err);
-              response.error(res,'Creation Error');
-            } else {
-              response.success(res);
-            }
-          });
-        }).catch((err)=>{
-          console.log(err);
-          response.error(res,'Creation Error');
-        });
-      } else {
-        response.error(res,'Invalid Parameters');
-      }
-    } else {
-      response.error(res,'Missing Parameters');
-    }
-  } else {
-    response.error(res,'Missing Body');
-  }
-}
-module.exports = {post}
-
-
-*/
 
 module.exports = {login,create};
