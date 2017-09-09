ad802d2a8078afcfdc38332e3ebfc3526bd24cf1
CalebBlack <caleb@polished.tech>
2017-09-09 15:41:54 -0600

    Update map.js

--- packages/react-scripts/template/server/routes/map.js
+++ packages/react-scripts/template/server/routes/map.js
@@ -1,4 +1,4 @@
 const createuser = require('./createuser');
 const login = require('./login');
 const birthday = require('./birthday');
-module.exports = {createuser,login}
+module.exports = {birthday,createuser,login}
