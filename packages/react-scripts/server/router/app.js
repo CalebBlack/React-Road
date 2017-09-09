ffd8fd4c414f6bd0fd0c6cfbf2c7b88d52a6f9e9
CalebBlack <caleb@polished.tech>
2017-09-08 20:32:04 -0600

    Update app.js

--- packages/react-scripts/server/router/app.js
+++ packages/react-scripts/server/router/app.js
@@ -1,5 +1,7 @@
-const routemap = require('./routemap.js');
+const routemap = require('./routes/routemap.js');
 function setupRouter(app) {
-  
+  for (var i; i < routemap.length; i++) {
+    app.get('')
+  }
 }
 module.exports = setupRouter
