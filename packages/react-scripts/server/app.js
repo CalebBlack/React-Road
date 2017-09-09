e7e033a01ac13cd730730190a070382b0c79aa35
CalebBlack <caleb@polished.tech>
2017-09-08 20:15:38 -0600

    Update app.js

--- packages/react-scripts/server/app.js
+++ packages/react-scripts/server/app.js
@@ -1,1 +1,12 @@
+const express = require('express');
 
+
+const app = express();
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
