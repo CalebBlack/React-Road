c04e907b7c62f305a8c8684c07168d12d99bc3c6
CalebBlack <caleb@polished.tech>
2017-09-09 15:24:08 -0600

    Update validateauthtoken.js

--- packages/react-scripts/template/server/functions/validateauthtoken.js
+++ packages/react-scripts/template/server/functions/validateauthtoken.js
@@ -1,4 +1,19 @@
 const decodeAuth = require('./decodeauth');
 modules.exports = function(models,req){
-  models.Session.find()
+  var auth = decodeAuth(req);
+  return new Promise((resolve,reject)=>{
+    if (!auth) {
+      reject('invalid auth');
+    } else {
+      models.Session.find({key:auth.username,secret:auth.password}).then(data=>{
+        if (data[0]) {
+          resolve(data[0]);
+        } else {
+          reject('token not found');
+        }
+      }).catch(err=>{
+        reject(err);
+      });
+    }
+  });
 }
