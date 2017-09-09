63f7ae4cafb093740a0f25b01ad56e7e2724025e
CalebBlack <caleb@polished.tech>
2017-09-08 22:57:48 -0600

    Update models.js

--- packages/react-scripts/template/server/models.js
+++ packages/react-scripts/template/server/models.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 // END OF PACKAGE IMPORTS
 const schemamap = require('./schemas/map');
 // END OF LOCAL IMPORTS
-
+mongoose.connect(config.mongoaddress || 'mongodb://localhost/test')
 var output = {};
 for (var schemaname in schemamap) {
   let modelName = schemaname.charAt(0).toUpperCase()+schemaname.substring(1)+'s';
