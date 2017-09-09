c85bdc3f23ee1f03dac25af9443e6de9236e103f
CalebBlack <caleb@polished.tech>
2017-09-09 02:48:33 -0600

    Update decodeauth.js

--- packages/react-scripts/template/server/functions/decodeauth.js
+++ packages/react-scripts/template/server/functions/decodeauth.js
@@ -2,9 +2,13 @@ module.exports = function(req) {
   if (!req || !req.headers || !req.headers.authorization || typeof req.headers.authorization !== "string") {
     return null;
   }
-  auth = req.headers.authorization;
+  var auth = req.headers.authorization;
   if (!auth.startsWith('Basic ')) {
     return null;
   }
-  return Buffer.from(auth.substring(6), 'base64').toString('utf8');
+  auth = Buffer.from(auth.substring(6), 'base64').toString('utf8').split(':');
+  if (auth.length !== 2) {
+    return null;
+  }
+  return {username:auth[0],password:auth[1]};
 }
