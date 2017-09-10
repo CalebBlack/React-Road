f50cd07a4a249bb44a9a2348f969f15a3132b075
CalebBlack <caleb@polished.tech>
2017-09-10 04:16:41 -0600

    Update app.js

--- packages/react-scripts/template/src/app.js
+++ packages/react-scripts/template/src/app.js
@@ -4,14 +4,8 @@ import './app.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id='app'>
+
       </div>
     );
   }
