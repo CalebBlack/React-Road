bd8c6ccde9141cf67ddf982658299016f78ef786
CalebBlack <caleb@polished.tech>
2017-09-11 01:36:24 -0600

    Update logout.js

--- packages/react-scripts/template/server/routes/logout.js
+++ packages/react-scripts/template/server/routes/logout.js
@@ -1,12 +1,12 @@
 const findInModel = require('../functions/findinmodel');
 function get(req,res,models,sanitize,user){
   findInModel(models.Session,{owner:user.username}).then(data=>{
-    if (data[0]) {
-      data[0].remove().then(()=>{
+    if (data) {
+      data.remove().then(()=>{
         res.success('Logged Out Successfully');
       }).catch(err=>{
         res.internal();
-      })
+      });
     } else {
       res.internal();
     }
