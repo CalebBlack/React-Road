3cdbdcf3f20ead2972cea1d1d8653c100cf197fd
CalebBlack <caleb@polished.tech>
2017-09-11 00:03:07 -0600

    Update login.js

--- packages/react-scripts/template/src/redux/actions/login.js
+++ packages/react-scripts/template/src/redux/actions/login.js
@@ -1,32 +1,15 @@
-import request from '../../functions/request';
+import secureRequest from '../../functions/securerequest';
 import * as statuses from '../loginstatuses';
 import {setLoginStatus} from '../actiontypes';
 import safeParse from '../../functions/safeparse';
 export function initialize(){
   return dispatch => {
     dispatchLoginStatus(dispatch,statuses.initializing);
-    if (localStorage.token) {
-      var token = safeParse(localStorage.token);
-      if (token) {
-        console.log('found token',token);
-        request('get','/api/validatetoken',[token.key,token.secret]).then(xhr=>{
-          console.log('status',xhr.status);
-          console.log('response',xhr.response);
-          var result = safeParse(xhr.response);
-          if (result) {
-            dispatchLoginStatus(dispatch,statuses.loggedIn);
-          } else {
-            dispatchLoginStatus(dispatch,statuses.loggedOut);
-          }
-        }).catch(err=>{
-          dispatchLoginStatus(dispatch,statuses.loggedOut);
-        });
-      } else {
-        dispatchLoginStatus(dispatch,statuses.loggedOut);
-      }
-    } else {
+    secureRequest('get','/api/validatetoken').then(()=>{
+      dispatchLoginStatus(dispatch,statuses.loggedIn);
+    }).catch(err=>{
       dispatchLoginStatus(dispatch,statuses.loggedOut);
-    }
+    });
   }
 }
 export function logout(){
