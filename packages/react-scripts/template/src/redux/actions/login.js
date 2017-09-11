567c577e62f2376e110fd9eeeab2a05e9b9a4457
CalebBlack <caleb@polished.tech>
2017-09-10 23:10:21 -0600

    Update login.js

--- packages/react-scripts/template/src/redux/actions/login.js
+++ packages/react-scripts/template/src/redux/actions/login.js
@@ -1,20 +1,73 @@
 import request from '../../functions/request';
-import statuses from '../loginstatuses';
+import * as statuses from '../loginstatuses';
 import {setLoginStatus} from '../actiontypes';
+import safeParse from '../../functions/safeparse';
 export function initialize(){
   return dispatch => {
-    if (localStorage.key && localStorage.secret) {
-      request('get','validatetoken').then(response=>{
-        console.log('response',response);
-      }).catch(err=>{
-        console.log('error',error);
-        setLoginStatus(dispatch,statuses.loggedOut);
-      })
+    dispatchLoginStatus(dispatch,statuses.initializing);
+    if (localStorage.token) {
+      var token = safeParse(localStorage.token);
+      if (token) {
+        console.log('found token',token);
+        request('get','/api/validatetoken',[token.key,token.secret]).then(xhr=>{
+          console.log('status',xhr.status);
+          console.log('response',xhr.response);
+          var result = safeParse(xhr.response);
+          if (result) {
+            dispatchLoginStatus(dispatch,statuses.loggedIn);
+          } else {
+            dispatchLoginStatus(dispatch,statuses.loggedOut);
+          }
+        }).catch(err=>{
+          dispatchLoginStatus(dispatch,statuses.loggedOut);
+        });
+      } else {
+        dispatchLoginStatus(dispatch,statuses.loggedOut);
+      }
     } else {
-      setLoginStatus(dispatch,statuses.loggedOut);
+      dispatchLoginStatus(dispatch,statuses.loggedOut);
     }
   }
 }
-function setLoginStatus(dispatch,status) {
+export function logout(){
+  return dispatch => {
+    dispatchLoginStatus(dispatch,statuses.loggedOut);
+  }
+}
+export function login(username,password) {
+  return dispatch => {
+    request('get','/api/login',[username,password]).then(xhr=>{
+      console.log('LOGIN RESPONSE',xhr.response);
+      var result = safeParse(xhr.response);
+      if (result) {
+        localStorage.token = JSON.stringify(result.details);
+        dispatchLoginStatus(dispatch,statuses.loggedIn);
+      } else {
+        dispatchLoginStatus(dispatch,statuses.loggedOut);
+      }
+    }).catch(err=>{
+      console.log('request error',err);
+      dispatchLoginStatus(dispatch,statuses.loggedOut);
+    });
+  }
+}
+export function signup(username,password,email) {
+  return dispatch => {
+    request('post','/api/createuser',null,{username,password,email}).then(xhr=>{
+      console.log('SIGNUP RESPONSE',xhr.response);
+      var result = safeParse(xhr.response);
+      if (result) {
+        localStorage.token = JSON.stringify(result.details);
+        dispatchLoginStatus(dispatch,statuses.loggedIn);
+      } else {
+        dispatchLoginStatus(dispatch,statuses.loggedOut);
+      }
+    }).catch(err=>{
+      console.log('request error',err);
+      dispatchLoginStatus(dispatch,statuses.loggedOut);
+    });
+  }
+}
+function dispatchLoginStatus(dispatch,status) {
   dispatch({type:setLoginStatus,status});
 }
