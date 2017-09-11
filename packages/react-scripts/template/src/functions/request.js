8dc0c84f208a9ff6e6b95c9b3a8ad78dd3600ffa
CalebBlack <caleb@polished.tech>
2017-09-10 23:22:03 -0600

    Create request.js

---
+++ packages/react-scripts/template/src/functions/request.js
@@ -0,0 +1,45 @@
+const request = function (method, url, auth, content) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
+    };
+    xhr.onerror = function () {
+      reject({
+        status: xhr.status,
+        statusText: xhr.statusText
+      });
+    };
+    if (auth) {
+      console.log('request auth',auth);
+      xhr.setRequestHeader('Authorization', generateAuthStrings(auth));
+    }
+    if (content) {
+      if (typeof content === 'object') {
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.send(JSON.stringify(content));
+      } else {
+        xhr.send(content);
+      }
+    } else {
+      xhr.send();
+    }
+  });
+}
+function generateAuthStrings(strings) {
+    console.log('gen strings',strings);
+    var string = strings.join(':');
+    console.log('prebasic string',string);
+    var hash = window.btoa(string);
+    console.log('hash',hash);
+    return 'Basic ' + hash;
+}
+export default request;
