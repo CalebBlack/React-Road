80093ab40d114df50674b5b0f5a9f35a10226176
CalebBlack <caleb@polished.tech>
2017-09-10 23:23:09 -0600

    Update app.js

--- packages/react-scripts/template/src/app.js
+++ packages/react-scripts/template/src/app.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
+import pagemap from './pages/map';
 import './app.css';
+import Header from './header';
+
+const pages = pagemap.map((page)=>{return (<Route exact path={page[1]} component={page[0]}/>)})
 
 class App extends Component {
   render() {
     return (
       <div id='app'>
-
+        <Header/>
+        <Switch>
+        {pages}
+        </Switch>
       </div>
     );
   }
 }
+
 
 export default App;
