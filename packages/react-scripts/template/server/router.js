31c5e4b4f7d5ab6d91e7a1a226d36bff19a9d558
CalebBlack <caleb@polished.tech>
2017-09-13 16:05:31 -0600

    Update router.js

--- packages/react-scripts/template/server/router.js
+++ packages/react-scripts/template/server/router.js
@@ -1,7 +1,7 @@
 const models = require('./models');
 const routemap = require('./routes/map');
 const secureroutemap = require('./routes/securemap');
-const setupResponse = require('./functions/response');
+const setupResponse = require('./functions/setupresponse');
 const config = require('./config');
 const APIRoute = config.APIRoute || '/api';
 const validateAuthToken = require('./functions/validateauthtoken');
@@ -9,15 +9,15 @@ const find = require('./functions/findinmodel');
 const sanitation = require('./functions/sanitize');
 const methods = config.methods || ['get','post','delete','put','patch'];
 const CompileFunction = require('./functions/compilefunctions');
-const setupBasicRoute = new CompileFunction(sanitation,models);
+const setupBasicRoute = function(req,res){return new CompileFunction(sanitation,models).chain};
 
 function setupRoute(responseFunction,secure=false){
-  var route = setupBasicRoute.compile(responseFunction);
+  var route = setupBasicRoute.chain(setupResponse(res)).compile(responseFunction);
   return (req,res)=>{
     if (secure){
       validateAuthToken(models,req).then(token=>{
         find(models.User,{username:token.owner}).then(user=>{
-            route(req,res,user,token);
+            setupBasicRoute.chain(req,res,user,token);
           }
         }).catch(err=>{
           res.internal(err);
