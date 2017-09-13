ca18698014b90d96dae531ce1d759a25294eb491
CalebBlack <caleb@polished.tech>
2017-09-13 16:42:31 -0600

    Update login.js

--- packages/react-scripts/template/server/routes/login.js
+++ packages/react-scripts/template/server/routes/login.js
@@ -1,15 +1,15 @@
 const userManager = require('../managers/users');
 
 const validateAuth = require('../functions/validateauth');
-function get(req,res,models) {
-  validateAuth(req,models).then(user=>{
-    userManager.login(models,user).then(token=>{
+function get(req) {
+  validateAuth(req,this.models).then(user=>{
+    userManager.login(this.models,user).then(token=>{
       this.success(this.sanitize(token,'token'));
     }).catch(err=>{
-      this.internal();
+      this.internal(err);
     });
   }).catch(err=>{
-    this.error(res,'Auth Validation Error');
+    this.error('Auth Validation Error');
   });
 }
 
