d65e319b95bf11855f6e6369774b44754366d328
CalebBlack <caleb@polished.tech>
2017-09-13 16:50:33 -0600

    Update setupresponse.js

--- packages/react-scripts/template/server/functions/setupresponse.js
+++ packages/react-scripts/template/server/functions/setupresponse.js
@@ -1,3 +1,18 @@
+function setup(res,logInternalErrors) {
+  var outSuccess = function(details,status){
+    success(res,details,status);
+  }
+  var outError = function(details,status){
+    error(res,details,status);
+  }
+  var outInternal = function(details,status) {
+    internal(res,details,logInternalErrors,status);
+  }
+  Object.defineProperty(outSuccess, "name", { value: "success" });
+  Object.defineProperty(outError, "name", { value: "error" });
+  Object.defineProperty(outInternal, "name", { value: "internal" });
+  return [outSuccess,outError,outInternal];
+}
 function success(res,details=null,status=200) {
   res.status(status);
   res.json({status:'success',details});
@@ -14,4 +29,4 @@ function internal(res,details=null,log = false,status=500){
   }
   res.json({status:'internal error',{})
 }
-module.exports = {success,error,internal};
+module.exports = {setup,success,error,internal};
