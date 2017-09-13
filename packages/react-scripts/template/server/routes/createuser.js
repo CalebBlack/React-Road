8f7f343ddf0879b7f8ed79b6f283eac58dfaebbc
CalebBlack <caleb@polished.tech>
2017-09-13 16:41:27 -0600

    Update createuser.js

--- packages/react-scripts/template/server/routes/createuser.js
+++ packages/react-scripts/template/server/routes/createuser.js
@@ -1,6 +1,6 @@
 const userManager = require('../managers/users');
 const response = require('../functions/response');
-function post(req,res,models,sanitation){
+function post(req,models,sanitation){
   if (req.body) {
     if (req.body.email && req.body.username && req.body.password ) {
       var userInput = sanitation(req.body,{email:'email',username:'username',password:'password'});
@@ -8,24 +8,21 @@ function post(req,res,models,sanitation){
       if (userInput.email && userInput.username && userInput.password) {
         userManager.signup(models,userInput.username,userInput.password,userInput.email).then(user=>{
           userManager.login(models,user).then(token=>{
-            console.log('token',sanitation(token,"token"));
-            res.success(sanitation(token,"token"));
+            this.success(sanitation(token,"token"));
           }).catch(err=>{
-            console.log(err);
-            res.internal();
+            this.internal(err);
           })
         }).catch(err=>{
-          console.log(err);
-          res.internal();
+          this.internal(err);
         });
       } else {
-        res.error('Invalid Parameters');
+        this.error('Invalid Parameters');
       }
     } else {
-      res.error('Missing Parameters');
+      this.error('Missing Parameters');
     }
   } else {
-    res.error('Missing Body');
+    this.error('Missing Body');
   }
 }
 module.exports = {post}
